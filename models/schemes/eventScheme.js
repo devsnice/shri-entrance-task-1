5e55bf154cbc3caed70350fb53e8b28f010a53b1
Borisov Semen <devsnice@gmail.com>
2017-12-17 18:42:02 +0800

    [*] - удалил самописную логику для получения вложенных моделей, sequelize добавляет свои методы, если модели имеют связи

--- models/schemes/eventScheme.js
+++ models/schemes/eventScheme.js
@@ -1,18 +1,11 @@
 const Sequelize = require('sequelize');
 
-const initScheme = function(sequelize, { Room }) {
-  if (!Room)
-    throw new Error("Needs dependency 'Room' for creating the model Event");
-
+const initScheme = function(sequelize) {
   const Event = sequelize.define('Event', {
     title: Sequelize.STRING,
     dateStart: Sequelize.DATE,
     dateEnd: Sequelize.DATE
   });
-
-  Event.prototype.getRoom = function() {
-    return Room.findById(this.RoomId);
-  };
 
   return Event;
 };
